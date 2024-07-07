32ee9580ce02af2ca8d14455222c41a63243170e
ZamurdFatima <fatimazamurd001@gmail.com>
2024-07-07 16:39:28 +0500

    operators
    Arithmetic, assignment, comparison, logical

---
+++ operators.py
@@ -0,0 +1,19 @@
+#Arithmetic
+a=7
+b=2
+                       # +, -
+print("a / b is" , a/b)
+print(a//b)  #double slash gives us the value of division without points or floating value
+print(a*b)
+print(a**b)  #double stars mean the value in exponent or power value.
+print(a%b)
+
+#Assignment
+a+=2                    # -=,*=, /=
+print(a)
+
+#Comparison
+print(a==b)             # !=, >=, <=
+
+#Logical
+print(a==b and a>b)     # and , or
