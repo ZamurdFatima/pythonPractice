f77e34df75b42058797a016a760868f13cfd8ddd
ZamurdFatima <fatimazamurd001@gmail.com>
2024-07-08 00:05:35 +0500

    Typecasting
    import

---
+++ typecasting.py
@@ -0,0 +1,56 @@
+# import pandas
+
+#df=pandas.read_excel("file.xlsx")
+#print(df)
+
+a=5
+print(a+1)
+b="5"
+
+print(int(b)+1)  # you can make the string type to int to perform a function
+
+# Practice set
+#...1
+a=3
+b=4
+print(a+b)
+
+#...2
+print(a/2)
+
+#...3                          //input function
+c=input("Enter number: ")
+d=int(c)                       # you can change the type
+print("my name is: ", {type(d)})
+
+#...4
+if(a>b):                            # if-else
+    {print('true')}
+else:
+    {
+        print('false')
+    }
+                                    # Comparison
+print(f"Is 'a' ({a}) greater than 'b' ({b})? {a > b}")
+
+#...5
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+result=(num1+num2)/2
+print("our result is: ",result)
+
+#...6
+n1=int(input("Enter an integer: "))
+print(n1*n1)
+
+
+
+
+
+
+
+
+
+
+
+
