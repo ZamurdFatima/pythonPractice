e2e867746d1d94817a2b8d993ec86a24e2d0b94e
ZamurdFatima <fatimazamurd001@gmail.com>
2024-07-17 13:18:01 +0500

    while loop

---
+++ while loop.py
@@ -0,0 +1,12 @@
+i= 0
+
+while(i<10):
+    print(i)
+    i+= 1
+while(True):
+    n=int(input("Enter number: "))
+    print(n)
+    if(n==0):
+        continue
+    print(n+1)
+print("Program has finished...")
