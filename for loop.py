7b63fa25af07a6ca197fe01cd26cea7eb41f71ac
ZamurdFatima <fatimazamurd001@gmail.com>
2024-07-17 13:15:16 +0500

    for loop
    for _ in range

---
+++ for loop.py
@@ -0,0 +1,20 @@
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+for i in range(4):
+    if(i==3):
+        break
+    print(i+1)
+
+print("Printing set...")
+a=[1,3,9,7]
+b={4,2,6}
+# a.sort()
+# c=sorted(b)
+# print(c)
+# print(a)
+for j in b:
+    print(j)
